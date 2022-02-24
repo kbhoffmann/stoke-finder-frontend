3f78791cb70230826e28b6c56ab716d447f4a1a9
Robin Dittrich <robinodittrich@gmail.com>
2022-02-24 07:57:54 -0800

    Add tests for welcome page login.

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,5 +7,18 @@ RSpec.describe 'Welcom Index Page' do
 
       expect(page).to have_content("Welcome to StokeFinder!")
     end
+
+    it 'allows the user to log in' do
+      # user_1 = User.create!(name: "David", email: "david@email.com", password: 'password123', password_confirmation: 'password123')
+      visit root_path
+
+      click_link 'Log In'
+      expect(current_path).to eq(login_path)
+
+      # fill_in(:email, with: 'robin@email.com')
+      # fill_in(:password, with: 'password123')
+      #
+      # click_button 'Log In'
+    end
   end
 end
