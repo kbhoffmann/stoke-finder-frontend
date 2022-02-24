4670940eff5c19675a57d1ae7a5a61aac4631440
Robin Dittrich <robinodittrich@gmail.com>
2022-02-24 07:57:20 -0800

    Add routes to login, register and dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  get '/dashboard', to: 'users#show'
+  delete '/logout', to: 'users#logout'
+
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login_user'
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
 end
