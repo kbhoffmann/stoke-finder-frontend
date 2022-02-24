c51dc5e841c06f336ff6b981fe44aa1207d71e54
Robin Dittrich <robinodittrich@gmail.com>
2022-02-24 08:50:31 -0800

    Add all actions for users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,23 @@ class UsersController < ApplicationController
   def index
   end
 
-  # def show
-  #   @user = User.find(session[:user_id])
-  # end
-  #
-  # def new
-  #   @user = User.new(user_params)
-  # end
+  def show
+    @user = User.find(session[:user_id])
+  end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     session[:user_id] = @user.id
-  #     redirect_to dashboard_path(@user.id)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def new
+    @user = User.new(user_params)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path(@user.id)
+    else
+      render :new
+    end
+  end
 
   def login_user
     user = User.find_by(email: params[:email])
@@ -35,10 +35,10 @@ class UsersController < ApplicationController
     end
   end
 
-  # def logout
-  #   session.destroy
-  #   redirect_to root_path
-  # end
+  def logout
+    session.destroy
+    redirect_to root_path
+  end
 
 private
 
