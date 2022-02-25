558a1c80d783149ade31d0c821bcaf498554f8d0
Robin Dittrich <robinodittrich@gmail.com>
2022-02-25 08:17:03 -0800

    Refactor schema to have password_digest.

--- db/schema.rb
+++ db/schema.rb
@@ -18,13 +18,13 @@ ActiveRecord::Schema.define(version: 2022_02_25_033352) do
   create_table "users", force: :cascade do |t|
     t.string "user_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.integer "access"
     t.string "street_address"
     t.string "city"
     t.string "state"
     t.string "zipcode"
-    t.string "activity_preference"
+    t.string "activity_preferences"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
