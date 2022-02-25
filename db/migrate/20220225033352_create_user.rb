99b595e0d5131ae844467ad1419abe53429ee181
Robin Dittrich <robinodittrich@gmail.com>
2022-02-25 08:16:23 -0800

    Refactor migrations for user table.

--- db/migrate/20220225033352_create_user.rb
+++ db/migrate/20220225033352_create_user.rb
@@ -3,13 +3,13 @@ class CreateUser < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :user_name
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.integer :access
       t.string :street_address
       t.string :city
       t.string :state
       t.string :zipcode
-      t.string :activity_preference
+      t.string :activity_preferences
 
       t.timestamps
     end
