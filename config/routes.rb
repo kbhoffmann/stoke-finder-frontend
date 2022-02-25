484947c34a2afa0740c2748a39c163005189a85e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-25 08:17:27 -0700

    add missing comma to route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   #from google authorization. This could go to whatever controller
   #we want.  User or Session maybe?  Just did routed this to 'user#create'
   #for now.
-  get '/auth/google_oauth2/callback' to: 'users#create'
+  get '/auth/google_oauth2/callback', to: 'users#create'
 end
