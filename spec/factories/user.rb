64092a7cea11c7e2198056e9fd8cded36226f721
Robin Dittrich <robinodittrich@gmail.com>
2022-02-25 08:17:22 -0800

    Add user factory attributes.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
-  # activities = ['hiking', 'camping', 'biking', 'fishing', 'paragliding']
   factory :user do
-    user_name { Faker::DcComics.name }
-    email { Faker::Internet.email }
-    access { Faker::Number.number(digits: 1) }
-    street_address { Faker::Address.street_address }
-    city { Faker::Address.city }
-    state { Faker::Address.state }
-    zipcode { Faker::Address.zip_code }
-    # activity_preference activities[rand(0,4)]
+    user_name {Faker::DcComics.name}
+    email {Faker::Internet.email}
+    password {Faker::DcComics.name}
+    access {0}
+    street_address {Faker::Address.street_address}
+    city {Faker::Address.city}
+    state {Faker::Address.state}
+    zipcode {Faker::Address.zip}
+    activity_preferences {"Hiking, Fishing, Boating"}
   end
 end
