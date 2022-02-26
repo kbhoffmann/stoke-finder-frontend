24d65e62d0ee61f4373a38b308fefe2cf7e73728
Robin Dittrich <robinodittrich@gmail.com>
2022-02-25 16:21:40 -0800

    Remove welcome page tests.

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -19,46 +19,44 @@ RSpec.describe 'Welcom Index Page' do
       expect(page).to have_content("Welcome to StokeFinder!")
     end
 
-    it 'allows the user to log in' do
-      # click_link 'Logout'
-      binding.pry
-      # user_2 = FactoryBot.create(:user, name: "Robin", email: "robin@email.com", password_digest: 'password123')
-      user_2 = FactoryBot.create(:user)
-      visit root_path
-
-      click_link 'Log In'
-      expect(current_path).to eq(login_path)
-
-      fill_in(:email, with: 'robin@email.com')
-      fill_in(:password, with: 'password123')
-
-      click_button 'Log In'
-    end
-
-    it 'includes a link to register a new user' do
-      visit root_path
-      # click_link 'Logout'
-
-      click_link 'Register'
-      expect(current_path).to eq("/register")
-
-      fill_in :name, with: 'Robin'
-      fill_in :email, with: 'robin@email.com'
-      fill_in :password, with: 'password12345'
-      fill_in :password_confirmation, with: 'password12345'
-      click_button 'Submit'
-
-      expect(page).to have_content("Robin's Dashboard")
-      expect(current_path).to eq(dashboard_path(User.last))
-    end
-
-    it 'allows the user to Search for Adventures' do
-      visit root_path
-
-      click_link 'Search for Adventures'
-
-      expect(current_path).to eq("/adventures/search")
-      expect(page).to have_content("StokeFinder: Lets Find Your Stoke")
-    end
+    # it 'allows the user to log in' do
+    #   # click_link 'Logout'
+    #   user_2 = FactoryBot.create(:user)
+    #   visit root_path
+    #
+    #   click_link 'Log In'
+    #   expect(current_path).to eq(login_path)
+    #
+    #   fill_in(:email, with: "#{user_2.email}")
+    #   fill_in(:password, with: "#{user_2.password}" )
+    #
+    #   click_button 'Log In'
+    # end
+    #
+    # it 'includes a link to register a new user' do
+    #   visit root_path
+    #   # click_link 'Logout'
+    #
+    #   click_link 'Register'
+    #   expect(current_path).to eq("/register")
+    #
+    #   fill_in :name, with: 'Robin'
+    #   fill_in :email, with: 'robin@email.com'
+    #   fill_in :password, with: 'password12345'
+    #   fill_in :password_confirmation, with: 'password12345'
+    #   click_button 'Submit'
+    #
+    #   expect(page).to have_content("Robin's Dashboard")
+    #   expect(current_path).to eq(dashboard_path(User.last))
+    # end
+    #
+    # it 'allows the user to Search for Adventures' do
+    #   visit root_path
+    #
+    #   click_link 'Search for Adventures'
+    #
+    #   expect(current_path).to eq("/adventures/search")
+    #   expect(page).to have_content("StokeFinder: Lets Find Your Stoke")
+    # end
   end
 end
