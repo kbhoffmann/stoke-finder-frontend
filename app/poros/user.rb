155b3c95e2ad60748035285e5bdbb6e6d2009c62
Robin Dittrich <robinodittrich@gmail.com>
2022-02-26 17:41:25 -0800

    Access user endpoint.

--- app/poros/user.rb
+++ app/poros/user.rb
@@ -1,9 +1,16 @@
 class User
-  attr_reader :id, :name
+  attr_reader :id, :user_name, :email, :street_address, :city, :state, :zipcode, :activity_preferences, :latitude, :longitude
 
   def initialize(data)
-    binding.pry
     @id = data[:id]
-    @name = data[:attributes][:name]
+    @user_name = data[:attributes][:user_name]
+    @email = data[:attributes][:email]
+    @street_address = data[:attributes][:street_address]
+    @city = data[:attributes][:city]
+    @state = data[:attributes][:state]
+    @zipcode = data[:attributes][:zipcode]
+    @activity_preferences = data[:attributes][:activity_preferences]
+    @latitude = data[:attributes][:latitude]
+    @longitude = data[:attributes][:longitude]
   end
 end
