e0641baef09087f0b81bc14bb79b4e6693cf82cf
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-03-02 12:33:15 -0700

    Add attributes to rec area poro

--- app/poros/rec_area.rb
+++ app/poros/rec_area.rb
@@ -1,14 +1,26 @@
 class RecArea
-  attr_reader :name,
-              :id,
+  attr_reader :id,
+              :name,
+              :fee_description,
               :description,
               :directions,
+              :latitude,
+              :longitude,
+              :email,
+              :phone,
+              :reservable,
               :activities
   def initialize(data)
-    @name = data[:RecAreaName]
     @id = data[:RecAreaID]
+    @name = data[:RecAreaName]
+    @fee_description = data[:RecAreaFeeDescription]
     @description = data[:RecAreaDescription]
     @directions = data[:RecAreaDirections]
+    @latitude = data[:RecAreaLatitude]
+    @longitude = data[:RecAreaLongitude]
+    @email = data[:RecAreaEmail]
+    @phone = data[:RecAreaPhone]
+    @reservable = data[:Reservable]
     @activities = data[:ACTIVITY]
   end
 end
