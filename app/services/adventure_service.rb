851b6cf4becf80fa4fe125b40a7757e2b3cec01c
John Napier <jcee.napier@gmail.com>
2022-03-03 19:31:52 -0700

    Remove pry

--- app/services/adventure_service.rb
+++ app/services/adventure_service.rb
@@ -4,7 +4,6 @@ class AdventureService
   end
 
   def self.adventure_create(attributes)
-    require 'pry'; binding.pry
     response = conn.post("/api/v1/adventures", attributes.to_json, "CONTENT_TYPE" => "application/json")
     JSON.parse(response.body, symbolize_names: true)
   end
